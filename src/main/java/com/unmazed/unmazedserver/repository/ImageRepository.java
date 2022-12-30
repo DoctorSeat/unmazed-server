0bd81e685d58df303ae08e2f6499d502f11e8731
Awar Satar <awar.satar@outlook.de>
2022-12-30 17:41:08 +0100

    file to BufferedImage from resources, first endpoint

--- src/main/java/com/unmazed/unmazedserver/repository/ImageRepository.java
+++ src/main/java/com/unmazed/unmazedserver/repository/ImageRepository.java
@@ -1,4 +1,25 @@
 package com.unmazed.unmazedserver.repository;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
 public class ImageRepository {
+    public BufferedImage loadBufferedImage(String imgName) throws IOException {
+        URL path = ImageRepository.class.getResource("/images/" + imgName + ".jpeg");
+        return ImageIO.read(path);
+
+    }
+
+    public static void main(String[] args)  {
+        ImageRepository repository = new ImageRepository();
+        try {
+            BufferedImage file = repository.loadBufferedImage("image1");
+            System.out.println(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/com/unmazed/unmazedserver/service/ImageTransferService.java
+++ src/main/java/com/unmazed/unmazedserver/service/ImageTransferService.java
@@ -1,4 +1,18 @@
 package com.unmazed.unmazedserver.service;
 
+import com.unmazed.unmazedserver.repository.ImageRepository;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+@Service
 public class ImageTransferService {
+    private ImageRepository imageRepository;
+    public BufferedImage loadImage(String imgName) throws IOException {
+        return imageRepository.loadBufferedImage(imgName);
+    }
 }
